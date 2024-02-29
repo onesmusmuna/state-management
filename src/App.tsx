eace853cf2159f977da59783c3edcc4fc7db6fe7
onesmusmuna <onesmusmuna@gmail.com>
2024-02-29 12:51:59 +0300

    decrement

--- src/App.tsx
+++ src/App.tsx
@@ -23,12 +23,15 @@ function App() {
 function Zudstand() {
   const counter = useCounter();
   const addOne = useCounter((state) => state.increment);
+  const minusOne = useCounter((state) => state.decrement);
+
   return (
     <>
       <h2>Zudstand State Management</h2>
       <p>{counter.count}</p>
 
       <button onClick={addOne}>Add One</button>
+      <button onClick={minusOne}>Minus One</button>
     </>
   );
 }
--- src/store.tsx
+++ src/store.tsx
@@ -3,10 +3,11 @@ import { create } from "zustand";
 interface ICounter {
   count: number;
   increment(): void;
-  //   decrement(): void;
+  decrement(): void;
 }
 
 export const useCounter = create<ICounter>((set) => ({
   count: 0,
   increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
 }));
