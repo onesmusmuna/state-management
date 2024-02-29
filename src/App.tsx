36401986cf260a920a9043284e8f7c86b07e5a37
onesmusmuna <onesmusmuna@gmail.com>
2024-02-29 13:04:20 +0300

    remove react usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
 import { useCounter } from "./store";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-
-      <hr />
-
       <ZudstandConter />
     </>
   );
